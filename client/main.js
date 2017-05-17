4ce2e56c7d04500964cf0f0c693443337e6d1423
Ilya Gavrilov <coder.ilya.gavrilov@gmail.com>
2017-05-17 13:13:40 +0000

    added routing

--- client/main.js
+++ client/main.js
@@ -6,111 +6,87 @@ import { Roles } from 'meteor/alanning:roles';
 import Tasks from "../shared/tasks.collection";
 import Attempts from "../shared/attempts.collection";
 import { moment } from 'meteor/momentjs:moment';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Meteor } from 'meteor/meteor';
 
 import './main.html';
 
-Template.main.onCreated(function(){
-  Session.set("registration", false);
+Meteor.startup(function(){
   Meteor.subscribe("users");
   Meteor.subscribe("tasks");
   Meteor.subscribe("attempts");
-});
-Template.main.helpers({
-  authorized(){
-    return Meteor.userId() != null;
-  },
-  registration(){
-    return Session.get("registration");
+})
+
+FlowRouter.route('/', {
+  name: 'home',
+  action: function(params){
+    if (Meteor.userId() != null)
+      FlowRouter.go("tasks.list");
+    else
+      FlowRouter.go("login");
   }
 });
+FlowRouter.route('/login', {
+  name: 'login',
+  action: function(params){
+    BlazeLayout.render("login")
+  }
+});
+FlowRouter.route('/register', {
+  name: 'register',
+  action: function(params){
+    BlazeLayout.render("register")
+  }
+});
+FlowRouter.route('/logout', {
+  name: 'logout',
+  action: function(params){
+    if (confirm("Вы точно хотите выйти?"))
+      Meteor.logout(() => FlowRouter.go('home'));
+    else
+      FlowRouter.go('home');
+  }
+});
+FlowRouter.route('/addTask', {
+  name: 'tasks.addTask',
+  action: function(params){
+    BlazeLayout.render("layout", {menu: "menu", scoreboard: "scoreboard", body: "addTask"})
+  }
+});
+FlowRouter.route('/tasks', {
+  name: "tasks.list",
+  action: function(params){
+    BlazeLayout.render("layout", {menu: "menu", scoreboard: "scoreboard", body: "tasks"})
+  }
+});
+FlowRouter.route('/tasks/:_id', {
+  name: "tasks.showTask",
+  action: function(params){
+    BlazeLayout.render("layout", {menu: "menu", scoreboard: "scoreboard", body: "showTask"});
+  }
+});
+FlowRouter.route('/tasks/:_id/edit', {
+  name: "tasks.editTask",
+  action: function(params){
+    BlazeLayout.render("layout", {menu: "menu", scoreboard: "scoreboard", body: "editTask"});
+  }
+});
+FlowRouter.route('/tasks/:_id/attempts', {
+  name: "tasks.taskAttempts",
+  action: function(params){
+    BlazeLayout.render("layout", {menu: "menu", scoreboard: "scoreboard", body: "taskAttempts"});
+  }
+});
+
+Template.registerHelper('isAdmin', function(){
+  return Roles.userIsInRole(Meteor.userId(), "admin");
+})
 Template.registerHelper("prettifyDate", function(timestamp) {
     return moment(timestamp).format('DD.MM.YYYY HH:mm:ss');
 });
-Template.registerForm.events({
-  'click #cancelRegistration'(event, instance){
-    Session.set("registration", false);
-  },
-  'click #register'(event, instance){
-    event.preventDefault();
-    var username = instance.$("[name='login']").val().trim();
-    var password = instance.$("[name='password']").val().trim();
-    if (username == "" || password == null)
-      return;
-    Meteor.call("register", { username, password })
-    Meteor.loginWithPassword(username, password)
-    Session.set("registration", false);
-  }
-})
-Template.loginForm.events({
-  'click #startRegistration'(event, instance){
-    Session.set("registration", true);
-  },
-  'click #login'(event, instance){
-    event.preventDefault();
-    var login = instance.$("[name='login']").val().trim();
-    var password = instance.$("[name='password']").val().trim();
-    if (login == "" || password == null)
-      return;
-    Meteor.loginWithPassword(login, password)
-  }
-})
-Template.app.onCreated(function(){
-  Session.set("currentTask", null);
-  if (window.history.state != null && window.history.state.currentTask != null)
-    Session.set("currentTask", window.history.state.currentTask);
-  window.onpopstate = function(){
-    Session.set("currentTask", null);
-    if (window.history.state != null && window.history.state.currentTask != null)
-      Session.set("currentTask", window.history.state.currentTask);
-  }
-  Session.set("adding", false);
-})
-Template.app.helpers({
-  tasks(){
-    return Tasks.find({}, {sort: {category: 1, cost: 1}});
-  },
-  users(){
-    return Meteor.users.find({}, {sort: { score: -1 }});
-  },
-  currentTask(){
-    return Tasks.findOne(Session.get("currentTask"));
-  },
-  admin(){
-    return Roles.userIsInRole(Meteor.userId(), "admin");
-  },
-  adding(){
-    return Session.get("adding");
-  },
-  taskCompletion(task){
-    if (task.opened === false) 
-      return "warning";
-    var result = Attempts.find({task: task._id, userId: Meteor.userId()}).count();
-    if (result == 0)
-      return "default";
-    else if (Attempts.findOne({task: task._id, userId: Meteor.userId(), success: true}) == null)
-      return "danger";
-    else
-      return "success";
-  },
-  isCurrentUser(user){
-    return user._id == Meteor.userId();
-  }
-});
-Template.app.events({
-  'click #exit'(event, instance){
-    event.preventDefault();
-    Meteor.logout();
-  },
-  'click .task-details'(event, instance){
-    event.preventDefault();
-    window.history.pushState({currentTask: instance.$(event.target).attr("data-id")}, "Задача", "/");
-    Session.set("currentTask", instance.$(event.target).attr("data-id"));
-  },
-  'click #tasks tbody tr td'(event, instance){
-    event.preventDefault();
-    window.history.pushState({currentTask: instance.$(instance.$(event.target).parent("tr")).attr("data-id")}, "Задача", "/");
-    Session.set("currentTask", instance.$(instance.$(event.target).parent("tr")).attr("data-id"));
-  },
+
+Template.addTask.events({
   'click #addTask'(event, instance){
     event.preventDefault();
     Meteor.call("addTask", {
@@ -123,64 +99,100 @@ Template.app.events({
       parent: instance.$("[name='taskParent']").val(),
       opened: instance.$("[name='taskOpened']").is(':checked')
     })
-    instance.$("[name='taskName']").val("");
-    instance.$("[name='taskDescription']").val("");
-    instance.$("[name='taskAttachment']").val("");
-    instance.$("[name='taskCategory']").val("");
-    instance.$("[name='taskFlag']").val("");
-    instance.$("[name='taskCost']").val("");
-    instance.$("[name='taskOpened']").prop("checked", false);
-    instance.$("[name='taskParent'] option").prop("selected", function() {
-      return this.defaultSelected;
-    });
-    Session.set("adding", false);
-  },
-  'click #adding'(event, instance){
-    event.preventDefault();
-    Session.set("adding", true);
-  },
+    FlowRouter.go('home');
+  }
 });
-Template.taskDetails.onRendered(function(){
-  Session.set("editing", false);
-  Session.set("fail", false);
-  Session.set("watchAttempts", false);
-})
-Template.taskDetails.helpers({
+Template.addTask.helpers({
+  tasks(){
+    return Tasks.find({}, {sort: {category: 1, cost: 1}});
+  }
+});
+
+Template.tasks.helpers({
   tasks(){
     return Tasks.find({}, {sort: {category: 1, cost: 1}});
   },
-  watchAttempts(){
-    return Session.get("watchAttempts");
-  },
-  attempts(){
-    return Attempts.find({task: this.task._id}, {sort: {timestamp: 1}})
-  },
-  editing(){
-    return Session.get("editing")
-  },
-  admin(){
-    return Roles.userIsInRole(Meteor.userId(), "admin");
-  },
-  taskCompletion(){
-    if (this.task.opened === false)
+  taskCompletion(task){
+    if (task.opened === false) 
       return "warning";
-    var result = Attempts.find({task: this.task._id, userId: Meteor.userId()}).count();
+    var result = Attempts.find({task: task._id, userId: Meteor.userId()}).count();
     if (result == 0)
       return "default";
-    else if (Attempts.findOne({task: this.task._id, userId: Meteor.userId(), success: true}) == null)
+    else if (Attempts.findOne({task: task._id, userId: Meteor.userId(), success: true}) == null)
+      return "danger";
+    else
+      return "success";
+  }
+});
+Template.tasks.events({
+  'click #tasks tbody tr td'(event, instance){
+    FlowRouter.go('tasks.showTask', {_id: instance.$(event.target).parent("tr").attr("data-id")})
+  },
+});
+
+Template.scoreboard.helpers({
+  users(){
+    return Meteor.users.find({}, {sort: { score: -1 }});
+  },
+  isCurrentUser(user){
+    return user._id == Meteor.userId();
+  }
+});
+
+Template.showTask.onRendered(function(){
+  Session.set("taskFailed", false);
+})
+Template.showTask.helpers({
+  taskCompletion(){
+    if (Tasks.findOne(FlowRouter.getParam('_id')).opened === false)
+      return "warning";
+    var result = Attempts.find({task: FlowRouter.getParam('_id'), userId: Meteor.userId()}).count();
+    if (result == 0)
+      return "default";
+    else if (Attempts.findOne({task: FlowRouter.getParam('_id'), userId: Meteor.userId(), success: true}) == null)
       return "danger";
     else
       return "success";
   },
   taskCompleted(){
-    return Attempts.find({task: this.task._id, userId: Meteor.userId(), success: true}).count() > 0;
+    return Attempts.find({task: FlowRouter.getParam('_id'), userId: Meteor.userId(), success: true}).count() > 0;
   },
   winners(){
-    var winners = Attempts.find({task: this.task._id, success: true}).map(attempt => attempt.user).join(", ");
+    var winners = Attempts.find({task: FlowRouter.getParam('_id'), success: true}).map(attempt => attempt.user).join(", ");
     return (winners == "") ? "Решивших нет. Станьте первым!" : "Решили: " + winners;
   },
-  fail(){
-    return Session.get("fail");
+  taskFailed(){
+    return Session.get("taskFailed");
+  },
+  task(){
+    return Tasks.findOne(FlowRouter.getParam('_id'));
+  }
+});
+Template.showTask.events({
+  'click #surrenderTask'(event, instance){
+    event.preventDefault();
+    Meteor.call("surrenderTask", {task: FlowRouter.getParam("_id"), flag: instance.$("[name='flag']").val()}, function(error, result){
+      if (!result)
+        Session.set("taskFailed", true);
+    })
+  },
+  'click .alert-dismissible .close'(event, instance){
+    event.preventDefault();
+    Session.set("fail", false);
+  }
+})
+
+Template.editTask.helpers({
+  task(){
+    return Tasks.findOne(FlowRouter.getParam('_id'));
+  },
+  tasks(){
+    return Tasks.find({}, {sort: {category: 1, cost: 1}});
+  },
+  taskOpened() {
+    if (this.task.opened)
+      return "checked";
+    return "";
   },
   taskWithoutParent() {
     if (this.task.parent)
@@ -192,37 +204,17 @@ Template.taskDetails.helpers({
       return "selected";
     return "";
   },
-  taskOpened() {
-    if (this.task.opened)
-      return "checked";
-    return "";
-  },
   isDifferentTask(task) {
     return this.task._id != task._id;
   }
 });
-Template.taskDetails.events({
-  'click #watchAttempts'(event, instance){
-    event.preventDefault();
-    Session.set("watchAttempts", true);
-  },
-  'click .removeAttempt'(event, instance){
-    event.preventDefault();
-    Meteor.call("removeAttempt", {attempt: instance.$(event.target).attr("data-id")})
-  },
+Template.editTask.events({
   'click #removeTask'(event, instance){
     event.preventDefault();
     if (confirm('Удалить задание?')) {
-      Meteor.call("removeTask", {task: Session.get("currentTask")});
-      Session.set("currentTask", null);
+      Meteor.call("removeTask", {task: FlowRouter.getParam("_id")});
+      FlowRouter.go('tasks.list');
     }
-  },
-  'click #surrenderTask'(event, instance){
-    event.preventDefault();
-    Meteor.call("surrenderTask", {task: Session.get("currentTask"), flag: instance.$("[name='flag']").val()}, function(error, result){
-      if (!result)
-        Session.set("fail", true);
-    })
   },
   'click #editTask'(event, instance){
     event.preventDefault();
@@ -237,14 +229,45 @@ Template.taskDetails.events({
       parent: instance.$("[name='taskParent']").val(),
       opened: instance.$("[name='taskOpened']").is(':checked')
     });
-    Session.set("editing", false);
-  },
-  'click #editing'(event, instance){
-    event.preventDefault();
-    Session.set("editing", true);
-  },
-  'click .alert-dismissible .close'(event, instance){
-    event.preventDefault();
-    Session.set("fail", false);
+    FlowRouter.go('tasks.showTask', {_id: FlowRouter.getParam("_id")});
   }
 });
+
+Template.taskAttempts.helpers({
+  task(){
+    return Tasks.findOne(FlowRouter.getParam('_id'));
+  },
+  attempts(){
+    return Attempts.find({task: FlowRouter.getParam('_id')}, {sort: {timestamp: 1}})
+  }
+});
+Template.taskAttempts.events({
+  'click .removeAttempt'(event, instance){
+    event.preventDefault();
+    Meteor.call("removeAttempt", {attempt: instance.$(event.target).attr("data-id")})
+  }
+})
+
+Template.register.events({
+  'click #register'(event, instance){
+    event.preventDefault();
+    var username = instance.$("[name='login']").val().trim();
+    var password = instance.$("[name='password']").val().trim();
+    if (username == "" || password == null)
+      return;
+    Meteor.call("register", { username, password }, function(){
+      Meteor.loginWithPassword(username, password, () => FlowRouter.go('home'));
+    });
+  }
+})
+
+Template.login.events({
+  'click #login'(event, instance){
+    event.preventDefault();
+    var login = instance.$("[name='login']").val().trim();
+    var password = instance.$("[name='password']").val().trim();
+    if (login == "" || password == null)
+      return;
+    Meteor.loginWithPassword(login, password, () => FlowRouter.go('home'));
+  }
+})
