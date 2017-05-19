f905598f01b916acf7731e00714b0cf3b754a891
Nikita Sychev <root@nsychev.ru>
2017-05-19 18:51:37 +0000

    added user page and invisibility options

--- client/templates/scoreboard/scoreboard.js
+++ client/templates/scoreboard/scoreboard.js
@@ -2,7 +2,10 @@ import { userCompare } from '/client/shared/userCompare';
 
 Template.scoreboard.helpers({
   users(){
-    return Meteor.users.find({}, {sort: userCompare});
+    return Meteor.users.find({visible: true}, {sort: userCompare});
+  },
+  invisibleUsers(){
+    return Meteor.users.find({visible: false}, {sort: {username: 1}})
   },
   needToShow(index, user){
     return (this.fullTable || index < 10 || user._id == Meteor.userId());
---
+++ client/templates/showUser/showUser.js
@@ -0,0 +1,26 @@
+import Attempts from "/shared/attempts.collection";
+import Tasks from "/shared/tasks.collection";
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+Template.showUser.helpers({
+  user(){
+    return Meteor.users.findOne(FlowRouter.getParam('_id'));
+  },
+  solutions(){
+    var solutions = Attempts.find({userId: FlowRouter.getParam('_id'), success: true}).map(attempt => Tasks.findOne(attempt.task).name).join(", ");
+    return (solutions == "") ? "Этот пользователь ещё ничего не решил" : "Решенные задания: " + solutions;
+  },
+  checkedIfVisible() {
+    if (Meteor.users.findOne(FlowRouter.getParam('_id')).visible)
+      return "checked";
+    return "";
+  }
+});
+Template.showUser.events({
+  "click input[name='showUser']": function(event, instance) {
+    Meteor.call("changeVisibility", {
+      userId: FlowRouter.getParam('_id'),
+      visibility: instance.$("input[name='showUser']").is(":checked")
+    });
+ }
+});
